05a3e0c928de6d881e811e820d9ab5b04658cec9
Ramon Barros <ramon@lideresistemas.com.br>
2020-07-28 16:32:23 -0300

    Comunicação entre Componentes Irmãos

--- componentes-comunicacao/comunicacao/src/components/Usuario.vue
+++ componentes-comunicacao/comunicacao/src/components/Usuario.vue
@@ -3,14 +3,18 @@
         <h1>Componente Usuário</h1>
         <p>Esse é um componente muito legal!</p>
         <p>Nome é <strong>{{ nome }}</strong></p>
+        <p>Idade é <strong>{{ idade }}</strong></p>
         <button @click="alterarNome">Alterar Nome</button>
         <hr>
         <div class="componentes">
             <app-usuario-info 
                 :nome="nome" 
+                :idade="idade"
                 @nomeMudou="nome = $event.antigo + $event.novo" 
                 :reiniciarFn="reiniciarNome" />
-            <app-usuario-editar />
+            <app-usuario-editar 
+                :idade="idade" 
+                @idadeMudou="idade = $event"/>
         </div>
     </div>
 </template>
@@ -23,7 +27,8 @@ export default {
     components: { AppUsuarioInfo, AppUsuarioEditar },
     data() {
         return {
-            nome: 'Pedro Silva'
+            nome: 'Pedro Silva',
+            idade: 21
         }
     },
     methods: {
