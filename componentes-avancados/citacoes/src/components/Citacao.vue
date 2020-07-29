18065954d3e92d0edf314c9d461ddddb43dee8e6
Ramon Barros <ramon@lideresistemas.com.br>
2020-07-28 21:34:32 -0300

    Utilizando multiplos slot e slot padrão.

---
+++ componentes-avancados/citacoes/src/components/Citacao.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="citacao">
+        <slot name="autor"></slot>
+        <slot name="texto"></slot>
+        <div class="fonte">
+            <slot name="fonte"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['texto']
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+    h1 {
+        color: red;
+    }
+    .fonte {
+        float: right;
+    }
+</style>
